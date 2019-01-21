4b2cc29971f61ff3fc59f7194e3bfc445064988c
Dhruv Patel <starb9979@gmail.com>
2019-01-21 12:39:40 -0500

    First commit by Dhruv, adding the Configuration App GUI

---
+++ src/TalkBoxConfigurationGUI/TalkBoxConfigApp.java
@@ -0,0 +1,12 @@
+package TalkBoxConfigurationGUI;
+
+import javax.swing.*;
+
+public class TalkBoxConfigApp
+{
+        public static void main(String [] args)
+        {
+            JFrame frame = new ConfigrationFrame();
+            frame.setVisible(true);
+        }
+    }
